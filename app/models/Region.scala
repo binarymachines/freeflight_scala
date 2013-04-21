ddb109f0037986da1add71c81163a22019258556
Dexter Taylor <dtaylor@theladders.com>
2013-04-21 11:53:41 -0400

    Add new actor types; set up basic state mechanism

--- app/models/Region.scala
+++ app/models/Region.scala
@@ -5,5 +5,5 @@ import play.api._, mvc._
 
 class LatLong(val latitude: Float, val longitude: Float)
 
-class Region(val northWestCorner: LatLong, val southEastCorner: LatLong, val name: String = "anonymous")
+class Region(val name: String = "anonymous", val northWestCorner: LatLong, val southEastCorner: LatLong, weatherConditions: MetConditions)
 
